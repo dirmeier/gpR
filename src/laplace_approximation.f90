51772a7358b5d4efe9bb41c4e1f977957664701e
dirmeier <simon.dirmeier@bsse.ethz.ch>
2016-09-17 01:34:40 +0200

    laplace approx additions

--- src/laplace_approximation.f90
+++ src/laplace_approximation.f90
@@ -4,10 +4,10 @@
       double precision, intent(in) :: c(n), K(n, n)
       double precision, intent(out) :: y(n), sig(n), D(n, n)i
 
-      double precision :: yold(n), sig(n), S(n, n), DIAG(n, n)
+      double precision :: yold(n), sig(n), S(n, n), DIAG(n, n), CURR(n, n), work(n)
       double precision, parameter :: thresh = 0.0000001
       integer, parameter niter :: 100000
-      integer :: i, iter = 1
+      integer :: ipiv(n), i, info, iter = 1
 
       external DGETRF
       external DGETRI
@@ -20,30 +20,15 @@
           sig = sigmoid(y, n)
           D = hessian(sig, n)
           yold = y
-          K = DIAG + MATMUL(D, K)
-          call DGETRF(n, n, Ainv, n, ipiv, info)
-          call DGETRI(n, Ainv, n, ipiv, work, n, info)
-          y = MATMUL(K, )
+          CURR = DIAG + MATMUL(D, K)
+          call DGETRF(n, n, CURR, n, ipiv, info)
+          call DGETRI(n, CURR, n, ipiv, work, n, info)
+          y = MATMUL(MATMUL(K, CURR), MATMUL(D, y) + c - sig)
           iter = iter + 1
       end do
 
       return
       end subroutine
-
-       while (base::mean(base::abs(y - y.old)) > 10e-5 && iter <= niter)
-    {
-      # compute class mapping for y
-      sig <- as.vector(sigmoid(y))
-      # compute Hessian of log-likelihood (ONLY IN THIS CASE WHERE THE LOG-TRANSFORM IS USED!)
-      D <- diag(sig*(1-sig))
-      y.old <- y
-      # compute Newton update
-      #  (see references: Barber p406, Equation~19.5.19; Rasmussen p43, Equation~3.18)
-      y <- base::as.vector(K.train %*%
-                             base::solve(diagn + D %*% K.train) %*%
-                             (D%*%y + c.train  - sig))
-      iter <- iter + 1
-    }
 
       pure function hessian(y, n)
       integer, intent(in) :: n
