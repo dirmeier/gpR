8289d78d2885ec1dafa55f498133970749ca06da
dirmeier <simon.dirmeier@bsse.ethz.ch>
2016-09-17 02:18:24 +0200

    laplace approx additions

--- src/laplace_approximation.f90
+++ src/laplace_approximation.f90
@@ -2,11 +2,11 @@
       implicit none
       integer, intent(in) :: n
       double precision, intent(in) :: c(n), K(n, n)
-      double precision, intent(out) :: y(n), sig(n), D(n, n)i
+      double precision, intent(out) :: y(n), sig(n), D(n, n)
 
-      double precision :: yold(n), sig(n), S(n, n), DIAG(n, n), CURR(n, n), work(n)
+      double precision :: yold(n), S(n, n), DIAG(n, n), CURR(n, n), work(n)
       double precision, parameter :: thresh = 0.0000001
-      integer, parameter niter :: 100000
+      integer, parameter :: niter = 100000
       integer :: ipiv(n), i, info, iter = 1
 
       external DGETRF
@@ -16,7 +16,7 @@
       forall(i = 1:n) DIAG(i, i) = 1
 
       yold = 1.0
-      do while (sum(abs(y - yold)) > thresh && iter < niter)
+      do while (sum(abs(y - yold)) > thresh .and. iter < niter)
           sig = sigmoid(y, n)
           D = hessian(sig, n)
           yold = y
@@ -30,10 +30,11 @@
       return
       end subroutine
 
-      pure function hessian(y, n)
+      function hessian(y, n)
+      implicit none
       integer, intent(in) :: n
       double precision, intent(in) :: y(n)
-      double precision, intent(out) :: hessian(n, n)
+      double precision :: hessian(n, n)
       integer :: i
       hessian (:, :) = 0.0
       do i = 1, n
@@ -41,10 +42,11 @@
       end do
       end function
 
-      pure function sigmoid(y, n)
-      integer intent(in):: n
-      double precision, intent(in) ::  y(n)
-      double precision, intent(out) :: sigmoid(n)
+      function sigmoid(y, n)
+      implicit none
+      integer, intent(in):: n
+      double precision, intent(in) :: y(n)
+      double precision :: sigmoid(n)
       integer :: i
       do i = 1, n
           sigmoid(i) = 1 / (1 + exp(y(i)))
